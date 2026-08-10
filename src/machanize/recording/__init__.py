fe7390d301401e7a02490dbfba5df232a53b1cba
Ali Uraish <aliuraishmirani@gmail.com>
2026-08-10 16:40:06 -0700

    Add episode recording support module, including EpisodeOutcome, EpisodeRecorder, and LeRobotDatasetSink for enhanced recording capabilities.

---
+++ src/machanize/recording/__init__.py
@@ -0,0 +1,6 @@
+"""Episode recording support."""
+
+from machanize.recording.lerobot_dataset import LeRobotDatasetSink
+from machanize.recording.recorder import EpisodeOutcome, EpisodeRecorder
+
+__all__ = ["EpisodeOutcome", "EpisodeRecorder", "LeRobotDatasetSink"]
