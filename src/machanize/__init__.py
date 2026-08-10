077cc0bdf9c6607d57ab50cc2f14feb6dda19903
Ali Uraish <aliuraishmirani@gmail.com>
2026-08-10 16:39:34 -0700

    Add initial machanize module with core components for robot supervision SDK.

---
+++ src/machanize/__init__.py
@@ -0,0 +1,6 @@
+"""Machanize robot supervision SDK."""
+
+from machanize.recording.recorder import EpisodeOutcome, EpisodeRecorder
+from machanize.runtime.bridge import MachanizeLeRobotBridge
+
+__all__ = ["EpisodeOutcome", "EpisodeRecorder", "MachanizeLeRobotBridge"]
